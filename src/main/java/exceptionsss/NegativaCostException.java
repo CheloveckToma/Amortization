7c37669dccc57644652d5206e436c10cd421a0b4
Cheloveck Toma <Begoli300>
2020-06-20 02:09:09 +0300

    add exceptions for calculations

---
+++ src/main/java/exceptionsss/NegativaCostException.java
@@ -0,0 +1,11 @@
+package exceptionsss;
+
+public class NegativaCostException extends Exception {
+    public NegativaCostException(String message) {
+        super();
+    }
+
+    public NegativaCostException() {
+
+    }
+}
---
+++ src/main/java/exceptionsss/ZeroCostException.java
@@ -0,0 +1,12 @@
+package exceptionsss;
+
+public class ZeroCostException extends Exception{
+
+    public ZeroCostException(String message) {
+        super();
+    }
+
+    public ZeroCostException() {
+
+    }
+}

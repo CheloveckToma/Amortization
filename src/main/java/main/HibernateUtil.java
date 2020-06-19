624a286361e2b6a42de9d28dec0284c8b4866f70
Cheloveck Toma <Begoli300>
2020-06-20 02:07:31 +0300

    create and connect Hibernate

---
+++ src/main/java/main/HibernateUtil.java
@@ -0,0 +1,54 @@
+package main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    /**
+     * Создает фабрику сессий
+     */
+
+    public static void buildSessionFactory() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("cfg/hibernate.cfg.xml").build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new ExceptionInInitializerError("Initial SessionFactory failed" + e);
+        }
+
+    }
+
+    /**
+     * @return возвращает фабрику сессий
+     */
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    /**
+     * Метод открывает новую сессию
+     *
+     * @return возвращает сессию
+     */
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    /**
+     * Метод закрывает сессию
+     */
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
